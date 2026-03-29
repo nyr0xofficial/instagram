b10f3931f05f2d82bbf6483a7e506cc5b7a577a8
jspkho <nyr0xofficial@gmail.com>
2026-03-29 23:20:55 +0200

    fix layout syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,4 +3,17 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Instagram",
-  description: "Connectez-vous pour partager des photos et des vidéos
+  description: "Connectez-vous pour partager des photos et des vidéos", // <-- Corrigé ici
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
